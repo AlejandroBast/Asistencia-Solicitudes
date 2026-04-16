149f481caa165c4b0599efde27486d97510b5276
AlejandroBast <nicohlas.personal@gmail.com>
2026-04-16 07:54:47 -0700

    fix de conflicto

--- taller_formularios/asgi.py
+++ taller_formularios/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for taller_formulario_KaledNicolas project.
+ASGI config for taller_formularios project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taller_formulario_KaledNicolas.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taller_formularios.settings')
 
 application = get_asgi_application()
--- taller_formularios/wsgi.py
+++ taller_formularios/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for taller_formulario_KaledNicolas project.
+WSGI config for taller_formularios project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taller_formulario_KaledNicolas.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taller_formularios.settings')
 
 application = get_wsgi_application()
