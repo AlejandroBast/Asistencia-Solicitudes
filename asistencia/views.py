8178542931d39f930488ce559d072f521d282ed0
AlejandroBast <nicohlas.personal@gmail.com>
2026-04-16 07:24:53 -0700

    asistencia formularios

---
+++ asistencia/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from .forms import AsistenciaForm
+
+def registro_asistencia(request):
+    if request.method == 'POST':
+        form = AsistenciaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('asistencia:confirmacion')
+    else:
+        form = AsistenciaForm()
+    return render(request, 'asistencia/registro.html', {'form': form})
+
+def confirmacion_asistencia(request):
+    return render(request, 'asistencia/confirmacion.html')
